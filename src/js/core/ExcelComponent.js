4e9b68047e14af1847e8c66dfcea5e739c6b47ab
Zakharovzvv <zakharovzvv@gmail.com>
2020-08-27 12:44:26 +0300

    test husky

--- src/js/core/ExcelComponent.js
+++ src/js/core/ExcelComponent.js
@@ -1,17 +1,20 @@
 import { DomListener } from './DomListener';
 
 export class ExcelComponent extends DomListener {
-	constructor($root,options={}) {
-		super($root,options.listeners);
-		this.name=options.name||''
+	constructor($root, options = {}) {
+		super($root, options.listeners);
+		this.name = options.name || '';
 	}
+
 	toHTML() {
 		return '';
 	}
-	init(){
-		this.initDOMListeners()
+
+	init() {
+		this.initDOMListeners();
 	}
-	destroy(){
-		this.removeDOMListeners()
-}
+
+	destroy() {
+		this.removeDOMListeners();
+	}
 }
